04fa9fab4b5805a1a84b8080113fa01d6732122f
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2021-10-20 12:22:11 +0700

    Procfile for FliterAndSearchCRUD.

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -15,7 +15,7 @@ class EventSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+//        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         DB::table('events')->truncate();
         DB::table('events')->insert([
             [
@@ -26,6 +26,7 @@ class EventSeeder extends Seeder
                 'endDate' => Carbon::now(),
                 'portfolio_id' => 1,
                 'ticketPrice' => 20000,
+                'thumbnail' => "https://image.thanhnien.vn/460x306/Uploaded/2021/fsmym/2021_10_01/giai-thuong-the-game-awards-2021-01_puwh.jpg?width=500",
                 'status' => 1,
             ],
             [
@@ -36,6 +37,7 @@ class EventSeeder extends Seeder
                 'endDate' => Carbon::now(),
                 'portfolio_id' => 2,
                 'ticketPrice' => 30000,
+                'thumbnail' => "https://site-cdn.givemesport.com/images/21/10/05/1ed5034e1eeee8c952d92c9a25f6f064/320.jpg",
                 'status' => 1,
             ],
             [
@@ -46,6 +48,7 @@ class EventSeeder extends Seeder
                 'endDate' => Carbon::now(),
                 'portfolio_id' => 3,
                 'ticketPrice' => 40000,
+                'thumbnail' => "https://cellphones.com.vn/sforum/wp-content/uploads/2021/10/The-Game-Awards-2019.jpg",
                 'status' => 2,
             ],
             [
@@ -56,6 +59,7 @@ class EventSeeder extends Seeder
                 'endDate' => Carbon::now(),
                 'portfolio_id' => 4,
                 'ticketPrice' => 50000,
+                'thumbnail' => "https://i.vietgiaitri.com/2021/10/2/the-game-awards-2021-cong-bo-ngay-ra-mat-bec25-6067652_default.jpg",
                 'status' => 3,
             ],
             [
@@ -66,9 +70,10 @@ class EventSeeder extends Seeder
                 'endDate' => Carbon::now(),
                 'portfolio_id' => 5,
                 'ticketPrice' => 60000,
+                'thumbnail' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1oZr_wcj5Mg1XZYSSgWR3emSE0Wts40izDQ&usqp=CAU",
                 'status' => 0,
             ],
         ]);
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+//        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
